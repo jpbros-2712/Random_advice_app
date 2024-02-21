c7c1c28a9372daa0b50a43943e980391aaae315e
jpbros-2712 <princejasakiya2712@gmail.com>
2024-02-21 20:44:03 +0530

    infinite calling of api fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useState } from "react";
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App()
-{
-    const [advice,setAdvice] = useState('');
+function App() {
+  const [advice, setAdvice] = useState("");
 
-    const fetchAdvice =  () => {
-        axios.get('https://api.adviceslip.com/advice')
-            .then((Response)=>{
-                const {advice} = Response.data.slip; // destructuring of abject
-                setAdvice(advice);
-            })
-            .catch((error)=>{
-                console.log(error);
-            });
-    }
-    
-    return (
-        <div className="app">
-            <div className="card">
-                {fetchAdvice()}
-                <h1 className="advice-data"> {advice}</h1>
-                <button className="btn" onClick={fetchAdvice}>Give Me Advice!!</button>
-            </div>
-        </div>
-    );
+  useEffect(()=>{
+    fetchAdvice();
+  },[]);
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((Response) => {
+        const { advice } = Response.data.slip; // destructuring of abject
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="advice-data">{advice}</h1>
+        <button className="btn" onClick={fetchAdvice}>
+          Give Me Advice!!
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
